c8adf335b6035a277055e2978855551da432c98d
Beatriz <beapnhr102@gmail.com>
2023-10-19 07:43:31 -0300

    build: component dynamic properties added

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css']
+})
+export class BigCardComponent implements OnInit{
+
+  @Input()
+  photoCover:String = ""
+  @Input()
+  photoAlt:String = ""
+  @Input()
+  cardTitle:String = ""
+  @Input()
+  cardDescription:String = ""
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+}
---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.css']
+})
+export class SmallCardComponent implements OnInit{
+  
+  @Input()
+  photoCover:String = ""
+  @Input()
+  photoAlt:String = ""
+  @Input()
+  cardTitle:String = ""
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+}
